5a070b0a1837d5b0b19b1bc16599a59d4317ed4a
kodluyoruz <info@kodluyoruz.org>
2022-04-16 21:38:30 +0300

    new update

--- config/keys.js
+++ config/keys.js
@@ -1,9 +1,3 @@
-// module.exports={
-//     googleProjectID: 'reactpageagent-dcbg',
-//     dialogFlowSessionID: 'react-bot-session',
-//     dialogFlowSessionLanguageCode:'en-US'
-// }
-
 if(process.env.NODE_ENV==='production'){
     module.exports=require('./prod');
 }else {
